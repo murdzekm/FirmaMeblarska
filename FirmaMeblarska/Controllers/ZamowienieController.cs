6567d2aabaedde01c79d0353c80ad40a676fd9d9
murdzekm <murdzekm@users.github.com>
2021-01-11 14:58:36 +0100

    zamówienie - add/edit

--- FirmaMeblarska/Controllers/ZamowienieController.cs
+++ FirmaMeblarska/Controllers/ZamowienieController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FirmaMeblarska.Models;
 using FirmaMeblarska.Data;
+using FirmaMeblarska.ViewModels;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace FirmaMeblarska.Controllers
 {
@@ -63,8 +65,186 @@ namespace FirmaMeblarska.Controllers
            
         }
 
-        
+        public IActionResult Create()
+        {
+            loadDDL();
+            var zamowienie = new Zamowienie();
+            zamowienie.ZamowieniePlyta = new List<ZamowieniePlyta>();
+            PopulateAssignedConditionData(zamowienie);
+            return View();
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ZamowienieId,DataZlozenia,Cena,StatusId,KlientId,ZespolId,AdresId")] Zamowienie zamowienie, string[] selectedConditions)
+
+        {
+            try
+            {
+                if (selectedConditions != null)
+                {
+                    zamowienie.ZamowieniePlyta = new List<ZamowieniePlyta>();
+                    foreach (var cond in selectedConditions)
+                    {
+                        var condToAdd = new ZamowieniePlyta { ZamowienieId = zamowienie.ZamowienieId, PlytaId = int.Parse(cond) };
+                        zamowienie.ZamowieniePlyta.Add(condToAdd);
+                    }
+                }
+
+                // UpdateZespolPracownik(selectedConditions, zespol);
+                if (ModelState.IsValid)
+                {
+                    _context.Add(zamowienie);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
+            PopulateAssignedConditionData(zamowienie);
+            return View(zamowienie);
+
+
+        }
+
+        // GET: Zespol/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            loadDDL();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zamowienie = await _context.Zamowienie
+                .Include(p => p.ZamowieniePlyta).ThenInclude(p => p.Plyta)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(p => p.ZamowienieId == id);
+            //.FindAsync(id);
+            if (zamowienie == null)
+            {
+                return NotFound();
+            }
+            PopulateAssignedConditionData(zamowienie);
+            return View(zamowienie);
+        }
+
+        // POST: Zespol/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]        
+        public async Task<IActionResult> Edit(int id, string[] selectedConditions)
+        {
+
+            var zamowienieToUpdate = await _context.Zamowienie
+                .Include(d => d.ZamowieniePlyta).ThenInclude(d => d.Plyta)
+                .SingleOrDefaultAsync(d => d.ZamowienieId == id);
+            //Check that you got it or exit with a not found error
+
+
+            if (zamowienieToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            UpdateZespolPracownik(selectedConditions, zamowienieToUpdate);
+
+            if (await TryUpdateModelAsync<Zamowienie>(zamowienieToUpdate, "",
+                d => d.Cena, d => d.DataZlozenia, d => d.KlientId, d => d.AdresId, d => d.ZespolId, d => d.StatusId))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (RetryLimitExceededException /* dex */)
+                {
+                    ModelState.AddModelError("", "Unable to save changes after multiple attempts. Try again, and if the problem persists, see your system administrator.");
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ZamowienieExists(zamowienieToUpdate.ZamowienieId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                }
+
+            }
+
+            PopulateAssignedConditionData(zamowienieToUpdate);
+            return View(zamowienieToUpdate);
+
+        }
+
+
+        private void PopulateAssignedConditionData(Zamowienie zamowienie)
+        {
+            var allConditions = _context.Plyta;
+            var pConditions = new HashSet<int>(zamowienie.ZamowieniePlyta.Select(b => b.PlytaId));
+            var viewModel = new List<PlytaVW>();
+            foreach (var con in allConditions)
+            {
+                viewModel.Add(new PlytaVW
+                {
+                    PlytaId = con.PlytaId,
+                    Nazwa = con.Kod,
+                    Assigned = pConditions.Contains(con.PlytaId)
+                });
+            }
+            ViewData["Conditions"] = viewModel;
+        }
+        private void UpdateZespolPracownik(string[] selectedConditions, Zamowienie zamowienieToUpdate)
+        {
+            if (selectedConditions == null)
+            {
+                zamowienieToUpdate.ZamowieniePlyta = new List<ZamowieniePlyta>();
+                return;
+            }
+
+            var selectedOptionsHS = new HashSet<string>(selectedConditions);
+            var docSpecialties = new HashSet<int>(zamowienieToUpdate.ZamowieniePlyta.Select(b => b.PlytaId));
+            foreach (var s in _context.Plyta)
+            {
+                if (selectedOptionsHS.Contains(s.PlytaId.ToString()))
+                {
+                    if (!docSpecialties.Contains(s.PlytaId))
+                    {
+                        zamowienieToUpdate.ZamowieniePlyta.Add(new ZamowieniePlyta
+                        {
+                            PlytaId = s.PlytaId,
+                            ZamowienieId = zamowienieToUpdate.ZamowienieId
+                        });
+                    }
+                }
+                else
+                {
+                    if (docSpecialties.Contains(s.PlytaId))
+                    {
+                        ZamowieniePlyta specToRemove = zamowienieToUpdate.ZamowieniePlyta.SingleOrDefault(d => d.PlytaId == s.PlytaId);
+                        _context.Remove(specToRemove);
+                    }
+                }
+            }
+        }
+
+        private bool ZamowienieExists(int id)
+        {
+            return _context.Zamowienie.Any(e => e.ZamowienieId == id);
+        }
+
+        /*
         // GET: Zamowienie/Create
         public IActionResult AddOrEdit(int id = 0)
         {
@@ -97,13 +277,13 @@ namespace FirmaMeblarska.Controllers
             }
             return View(zamowienie);
         }
+        */
 
-       
 
-        
+
 
         // GET: Zamowienie/Delete/5
-       
+
         public async Task<IActionResult> Delete(int? id)
         {
             var czesc = await _context.Pracownik.FindAsync(id);
